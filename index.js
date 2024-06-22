b9458fd2d5ff4b4e5eb20583324137cce6c0e6a2
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-06-22 03:47:20 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const marks = await inquirer.prompt([
+    {
+        name: "maxMarks",
+        type: "number",
+        message: "enter total max Marks"
+    },
+    {
+        name: "obtainedMarks",
+        type: "number",
+        message: "enter total obtained marks"
+    }
+]);
+const per = marks.maxMarks / marks.obtainedMarks;
+const total = per * 100;
+const result = Math.round;
+console.log(`${total.toFixed(2)}%`);
